42f8ab5b08383b94685dfba9503e8ca2fe51c5f3
LorenzoLaudato <75116880+LorenzoLaudato@users.noreply.github.com>
2022-11-14 16:22:02 +0100

    Update evaluate.py

--- MM-USENet/evaluate.py
+++ MM-USENet/evaluate.py
@@ -7,15 +7,6 @@ import torch.nn as nn
 
 from utils.dice_score import multiclass_dice_coeff, dice_coeff, dice_loss
 
-def DICE_COE(mask1, mask2):
-    intersect = np.sum(mask1*mask2)
-    fsum = np.sum(mask1)
-    ssum = np.sum(mask2)
-    dice = (2 * intersect ) / (fsum + ssum)
-    dice = np.mean(dice)
-    dice = round(dice, 3) # for easy reading
-    return dice 
-
 def evaluate(batch_size,net, dataloader, device, criterion):
     net.eval()
     num_val_batches = len(dataloader)
